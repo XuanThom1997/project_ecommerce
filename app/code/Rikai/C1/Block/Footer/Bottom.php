e55f9316acf044a6bdd681ff02783635ef139116
xuanthom <nguyenxuanthom15tclc1@gmail.com>
2021-10-19 09:13:10 +0700

    create footer

---
+++ app/code/Rikai/C1/Block/Footer/Bottom.php
@@ -0,0 +1,12 @@
+<?php
+namespace Rikai\C1\Block\Footer;
+
+use Magento\Framework\View\Element\Template;
+
+class Bottom extends Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+}
